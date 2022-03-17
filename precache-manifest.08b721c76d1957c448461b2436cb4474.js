58d662f0c5a46c6da3fb4e3da7703e945f4d633e
unknown <a.savu.stefan@gmail.com>
2022-03-17 15:31:22 +0200

    Updates

---
+++ precache-manifest.08b721c76d1957c448461b2436cb4474.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "133d89c2f012cd05df55",
+    "url": "/tesla/static/css/main.e9cb9137.chunk.css"
+  },
+  {
+    "revision": "133d89c2f012cd05df55",
+    "url": "/tesla/static/js/main.1df878e7.chunk.js"
+  },
+  {
+    "revision": "d20af9c0bdbaae104f92",
+    "url": "/tesla/static/js/runtime~main.84915f9e.js"
+  },
+  {
+    "revision": "5d09b615ec8d41b244c5",
+    "url": "/tesla/static/js/2.f97791a1.chunk.js"
+  },
+  {
+    "revision": "a893cbb6517b305d0fcf80dfc152715a",
+    "url": "/tesla/index.html"
+  }
+];
